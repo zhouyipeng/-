75a39609eb7434891266ae52a4c2d36837798ee0
zhouyipeng <1141022724@qq.com>
2019-04-16 20:06:42 +0800

    Create 分数分级.py

---
+++ 分数分级.py
@@ -0,0 +1,10 @@
+score = eval(input())
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+print("输入成绩输入级别{}".format(grade))
