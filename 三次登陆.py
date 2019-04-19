23c83cc842009f25c85a8f09a39eeafe35218841
zhouyipeng <1141022724@qq.com>
2019-04-19 19:24:20 +0800

    Create 三次登陆.py

---
+++ 三次登陆.py
@@ -0,0 +1,12 @@
+count = 0
+while count < 3:
+    name = input()
+    password = input()
+    if name == 'Kate'and password == '666666':
+        print("登陆成功")
+        break
+    else:
+        count += 1
+        print("用户名或密码输入错误")
+        if count == 3:
+            print("3次用户名或者密码均有误！退出程序。")
